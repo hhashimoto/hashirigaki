7ca41fbe84f00ed8f88026bf02c7f7fe108f146e
hhashimoto <almite.bridge@gmail.com>
2021-05-04 16:33:41 +0900

    Added social login by github account

--- Gemfile
+++ Gemfile
@@ -57,3 +57,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'mysql2'
 gem 'devise'
+gem 'omniauth-github'
+gem 'omniauth-rails_csrf_protection'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
+  devise_scope :user do
+    get 'sign_in', to: 'users/sessions#new'
+  end
+
   root 'scraps#index'
 
   get '/scraps', to: 'scraps#index'
